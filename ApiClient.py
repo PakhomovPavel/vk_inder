844e074008384047cafad6f1a725480f862481d8
PakhomovPavel <125836486+PakhomovPavel@users.noreply.github.com>
2023-02-24 03:22:50 +0300

    Add files via upload

--- ApiClient.py
+++ ApiClient.py
@@ -2,14 +2,11 @@
 """
 https://oauth.vk.com/authorize?client_id=вашегоприложения&display=page&scope=stats.offline&response_type=token&v=5.131
 """
-
 from config import user_token
 import requests
 from pprint import pprint
 import datetime
 data = datetime.date.today().year
-
-
 
 class VkApiClient:
     def __init__(self, user_token: str, api_version: str, base_url: str = "https://api.vk.com/"):
@@ -23,46 +20,52 @@ class VkApiClient:
             "v": self.api_version,
         }
 
-
-    def get_info(self, user_ids,):
+    def get_info(self, user_ids):
         params = {
             "user_ids": user_ids,
             "fields": 'bdate, sex, city, relation, is_closed, can_access_closed'
-
         }
         try:
             return requests.get(f"{self.base_url}/method/users.get",
-                                params={**params, **self.general_params()}).json()['response']
+                                params={**params, **self.general_params()}).json()
         except:
-            print("Проверте токен https://oauth.vk.com/authorize?client_id= вставьте id приложения &display=page&scope=stats.offline&response_type=token&v=5.131")
-
-
-
-
+            print("Проверте токен https://oauth.vk.com/authorize?client_id=51491184&display=popup&scope=stats.offline&response_type=token&v=5.131")
 
     def name_users(self, user_ids):
-        self.name = self.get_info(user_ids)[0]['last_name']
-        self.first_name = self.get_info(user_ids)[0]['first_name']
-        return f"{self.name} {self.first_name}"
-
+        try:
+            self.name = self.get_info(user_ids)['response'][0]['last_name']
+            self.first_name = self.get_info(user_ids)['response'][0]['first_name']
+            return f"{self.name} {self.first_name}"
+        except:
+            print('неудалось определить имя')
 
     def bdate_info(self, user_ids):
-        bdate = self.get_info(user_ids)[0]['bdate']
-        return bdate
-
-
+        try:
+            bdate = self.get_info(user_ids)['response'][0]['bdate']
+            return bdate
+        except:
+            return 'неудалось определить возраст'
 
     def sex_info(self, user_ids):
-        sex = self.get_info(user_ids)[0]['sex']
-        return sex
+        try:
+            sex = self.get_info(user_ids)['response'][0]['sex']
+            return sex
+        except:
+            print('неудалось определить пол')
 
     def city_info(self, user_ids):
-        city = self.get_info(user_ids)[0]['city']['title']
-        return city
+        try:
+            city = self.get_info(user_ids)['response'][0]['city']['title']
+            return city
+        except:
+            print('неудалось определить город')
 
     def relation(self, user_ids):
-        relation = self.get_info(user_ids)[0]['relation']
-        return relation
+        try:
+            relation = self.get_info(user_ids)['response'][0]['relation']
+            return relation
+        except:
+            print('неудалось определить статус')
 
     def all_city(self, city):
         params = {
@@ -72,11 +75,8 @@ class VkApiClient:
         try:
             return requests.get(f"{self.base_url}/method/database.getCities",
                                 params={**params, **self.general_params()}).json()['response']['items'][0]['title']
-        except IndexError:
-            return
-
-
-
+        except KeyError:
+            print('Проверте токен https://oauth.vk.com/authorize?client_id=51491184&display=popup&scope=stats.offline&response_type=token&v=5.131')
 
     def users_search(self, user_ids, min_age, max_age, city, offset=0):
         if self.get_info(user_ids)[0]['sex'] == 2:
@@ -86,26 +86,29 @@ class VkApiClient:
         hometown = city
         offset = offset
         params = {
-            "sort": 0,
-            "offset": offset,
-            "count": 1,
-            "hometown": hometown,
-            "sex": sex,
-            "status": 6,
-            "age_from": min_age,
-            "age_to": max_age,
-            "has_photo": 1,
-            "fields": 'city, hometown',
+        "sort": 0,
+        "offset": offset,
+        "count": 10,
+        "hometown": hometown,
+        "sex": sex,
+        "status": 6,
+        "age_from": min_age,
+        "age_to": max_age,
+        "has_photo": 1,
+        "fields": 'city, hometown',
         }
-
-        for profile in requests.get(f"{self.base_url}/method/users.search",
-                            params={**params, **self.general_params()}).json()['response']['items']:
-            if profile['is_closed'] == False and profile['can_access_closed'] == True:
-                profile_id = profile['id']
-                name = [profile['first_name'], profile['last_name']]
-                return [profile_id, name, self.photo_search(profile_id)]
-
-
+        try:
+            search_list = []
+            for profile in requests.get(f"{self.base_url}/method/users.search",
+                                params={**params, **self.general_params()}).json()['response']['items']:
+                if profile['is_closed'] == False and profile['can_access_closed'] == True:
+                    profile_id = profile['id']
+                    name = [profile['first_name'], profile['last_name']]
+                    photo = self.photo_search(profile_id)
+                    search_list.append([profile_id, name, photo])
+            return search_list
+        except KeyError:
+            print("Проверте токен https://oauth.vk.com/authorize?client_id=51491184&display=popup&scope=stats.offline&response_type=token&v=5.131")
 
     def photo_search(self, owner_id):
         params = {
@@ -119,22 +122,49 @@ class VkApiClient:
         result = []
         try:
             for i in requests.get(f"{self.base_url}/method/photos.get",
-                                params={**params, **self.general_params()}).json()['response']['items']:
+                                  params={**params, **self.general_params()}).json()['response']['items']:
+                photo_id = i['id']
                 popular.append(i['comments']['count'] + i['likes']['count'])
                 likes = i['comments']['count'] + i['likes']['count']
-                url = i['sizes'][-1]['url']
-                dict = {likes: url}
+                dict = {likes: photo_id}
                 result.append(dict)
 
             photo = []
             sort_popular = sorted(popular, reverse=True)[0:3]
-            for key in sort_popular:
+            for key in set(sort_popular):
                 for values in result:
                     if values.get(key) != None:
-                        photo.append(values.get(key))
+                        photo.append(f'photo{owner_id}_{values.get(key)}')
             return photo
-        except:
-           pass
+        except KeyError:
+            print("Проверте токен https://oauth.vk.com/authorize?client_id=51491184&display=popup&scope=stats.offline&response_type=token&v=5.131")
+
+
 
 
 vk_client = VkApiClient(user_token=user_token, api_version="5.131")
+
+
+
+
+
+
+
+
+
+
+# print(vk_client.all_city('Москва'))
+
+# pprint(vk_client.get_info(1))
+# pprint(vk_client.name_users(362420338))
+# print(vk_client.users_search(21199458,19,20,'Москва', offset=0))
+# pprint(vk_client.get_info(230574022))
+# print(vk_client.get_info(23556)[0]['city']['title'])
+# print(vk_client.city_info(21199458))
+# pprint(vk_client.all_city('москва'))
+# pprint(datetime.date.today().year)
+# pprint(vk_client.bdate_info(1))
+# pprint(vk_client.bdate_info(4))
+# print(vk_client.photo_search(370980424))
+# 289612462
+# 27424
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ApiClient import data
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from config import group_token
@@ -12,7 +13,6 @@ engine = sqlalchemy.create_engine(DSN)
 create_tables(engine)
 Session = sessionmaker(bind=engine)
 session_bd = Session()
-
 
 
 def listen():
@@ -38,8 +38,10 @@ def last_listen():
 
 
 def send_msg(user_id, message):
-    session_vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': 0})
+    session_vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': 0})
 
+def send_msg_photo(user_id, message, attachment = list):
+    session_vk.method('messages.send', {'user_id': user_id, 'message': message,'attachment': attachment,'random_id': 0})
 
 def main(request, user_id):
     send_msg(user_id, f"Привет {vk_client.name_users(user_id)}\n"
@@ -54,17 +56,17 @@ def main(request, user_id):
         else:
             return main(request, user_id)
 
-
 def start(user_id):
     if vk_client.city_info(user_id) != None:
-        send_msg(user_id,f"ваш город {vk_client.city_info(user_id)}(да,нет)?")
+        send_msg(user_id, f"ваш город {vk_client.city_info(user_id)} 'да,нет'?")
         for request in last_listen():
             if request == "да":
-                return search_age_min(user_id, city=vk_client.city_info(user_id))
+                return search_age(user_id, city=vk_client.city_info(user_id))
             elif request == "нет":
-                send_msg(user_id,"введите город в котором будем искать")
+                send_msg(user_id, "введите город в котором будем искать")
                 return definition_city(user_id)
-    else: return definition_city(user_id)
+    else:
+        return definition_city(user_id)
 
 def definition_city(user_id):
     for request in last_listen():
@@ -74,8 +76,17 @@ def definition_city(user_id):
             return definition_city(user_id)
         else:
             send_msg(user_id, f"выбран город: {vk_client.all_city(city)}")
-            return search_age_min(user_id, city=vk_client.all_city(city))
+            return search_age(user_id, city=vk_client.all_city(city))
 
+def search_age(user_id, city):
+    age_user = vk_client.bdate_info(user_id)
+    if age_user == 'неудалось определить возраст':
+        return search_age_min(user_id, city)
+    else:
+        age_user = int(age_user[6:])
+        min_age = data - age_user - 3
+        max_age = data - age_user + 3
+        return search(user_id, min_age, max_age, city, offset=0)
 
 def search_age_min(user_id, city):
     send_msg(user_id, f"Укажите минимальный возраст поиска, но не меньше 18 лет")
@@ -94,7 +105,6 @@ def search_age_min(user_id, city):
         except ValueError:
             send_msg(user_id, f"возраст должен содержать только цифры")
             return search_age_min(user_id, city)
-
 
 def search_age_max(user_id, min_age, city):
     send_msg(user_id, f"Укажите максимальный возраст поиска, но не больше 99 лет")
@@ -119,29 +129,27 @@ def search_age_max(user_id, min_age, city):
 
 def search(user_id, min_age, max_age, city, offset):
     candidat = vk_client.users_search(user_id, min_age, max_age, city, offset=offset)
-    if candidat == None:
-        return search(user_id, min_age, max_age, city, offset=offset + 1)
-    else:
-        candidat_id = candidat[0]
-        candidat_name = f"{candidat[1][-1]} {candidat[1][0]}"
-        candidat_photo = candidat[2]
+
+    for element in candidat:
+        candidat_id = element[0]
+        candidat_name = f"{element[1][-1]} {element[1][0]}"
+        candidat_photo = element[2]
+
         resl_id = []
         use_id = []
         for cand_res in session_bd.query(Search_result.id, Search_result.user_bot_id).all():
             resl_id.append(cand_res[0])
             use_id.append(cand_res[1])
         if (candidat_id in resl_id and user_id in use_id) != True:
-            send_msg(user_id, f"https://vk.com/id{candidat_id}\n{candidat_name}\n{candidat_photo}\nЧто бы перейти к следующей анкете напиши 'да'")
             BD_commit(user_id, candidat_id, candidat_name)
+            send_msg_photo(user_id, f"https://vk.com/id{candidat_id}\n{candidat_name}\nЧто бы перейти к следующей анкете напиши 'да'", candidat_photo)
             for request in last_listen():
                 if request == "да":
-                    return search(user_id, min_age, max_age, city, offset=offset + 1)
+                    return search(user_id, min_age, max_age, city, offset)
                 else:
                     return session_bd.close()
         else:
-            return search(user_id, min_age, max_age, city, offset=offset + 1)
-
-
+            search(user_id, min_age, max_age, city, offset=offset + 1)
 
 def BD_commit(user_id, candidat_id, candidat_name):
     bd_user = User_bot(id=user_id, name=vk_client.name_users(user_id))
@@ -155,7 +163,5 @@ def BD_commit(user_id, candidat_id, candidat_name):
     with session_bd.begin():
         session_bd.add(bd_result)
         session_bd.commit()
-
-
 
 listen()
